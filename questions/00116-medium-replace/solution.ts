b673aa5f5a0c6cd211b0b09fa0d36030175e7912
iamkanguk97 <iamkanguk97@kakao.com>
2024-10-29 17:01:59 +0900

    Edit TestCase namespace

--- questions/00116-medium-replace/solution.ts
+++ questions/00116-medium-replace/solution.ts
@@ -1,3 +1,5 @@
+import { Equal, Expect } from "../../utils"
+
 namespace iamkanguk_replace {
     export type Replace<S extends string, From extends string, To extends string> = |
         From extends ''
@@ -11,4 +13,8 @@ namespace TestCase {
     type TC1 = iamkanguk_replace.Replace<'foobar', 'bar', 'foo'>;   // foofoo
     type TC2 = iamkanguk_replace.Replace<'foobar', 'foo', 'bar'>;   // barbar
     type TC3 = iamkanguk_replace.Replace<'foobar', 'baz', 'foo'>;   // foobar
+
+    type R1 = Expect<Equal<TC1, 'foofoo'>>;
+    type R2 = Expect<Equal<TC2, 'barbar'>>;
+    type R3 = Expect<Equal<TC3, 'foobar'>>;
 }
